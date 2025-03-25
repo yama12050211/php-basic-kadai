fb8e2a384eb3998b46311d06c99df20e011473ae
yama12050211 <paa381205@gmail.com>
2025-03-26 01:22:12 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <titlet></title>
+</head>
+<body>
+    <p>
+        <?php
+        $score = 80+60+55+40+100+25+80+95+30+60;
+        echo $score/10;
+        ?>
+    </p>
+</body>
+</html>
