d3197d493794f5b1f48df2fb29dd2bb09ea0bc75
yama12050211 <paa381205@gmail.com>
2025-03-27 01:04:38 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <titlet></title>
+</head>
+<body>
+    <p>
+        <?php
+        $data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+        print_r($data);
+        ?>
+    </p>
+</body>
+</html>
